cfc61576e1e5be4bf0bb0b4d050353c58ff21b22
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2023-01-25 22:57:38 -0800

    Kuro no Kiseki 2 support (preliminary)

--- kuro_mdl_import_meshes.py
+++ kuro_mdl_import_meshes.py
@@ -21,7 +21,8 @@ from kuro_mdl_export_meshes import *
 def make_pascal_string(string):
     return struct.pack("<B", len(string)) + string.encode("utf8")
 
-def insert_model_data (mdl_data, material_section_data, mesh_section_data):
+# Primitive data is in Kuro 2.  In Kuro 1, it will be an empty buffer.
+def insert_model_data (mdl_data, material_section_data, mesh_section_data, primitive_section_data):
     with io.BytesIO(mdl_data) as f:
         new_mdl_data = f.read(12) #Header
         while True:
@@ -37,13 +38,15 @@ def insert_model_data (mdl_data, material_section_data, mesh_section_data):
                 section = material_section_data
             if section_info["type"] == 1: # Mesh section to replace
                 section = mesh_section_data
+            if section_info["type"] == 4: # Primitive section to replace
+                section = primitive_section_data
             new_mdl_data += section
         # Catch the null bytes at the end of the stream
         f.seek(current_offset,0)
         new_mdl_data += f.read()
         return(new_mdl_data)
 
-def build_material_section (mdl_filename):
+def build_material_section (mdl_filename, kuro_ver = 1):
     # Will read data from JSON file, or load original data from the mdl file if JSON is missing
     try:
         material_struct = read_struct_from_json(mdl_filename + "/material_info.json")
@@ -62,7 +65,11 @@ def build_material_section (mdl_filename):
         texture_block_count = 0
         for j in range(len(material_struct[i]['textures'])):
             texture_blocks += make_pascal_string(material_struct[i]['textures'][j]['texture_image_name']) \
-                + struct.pack("<3I", material_struct[i]['textures'][j]['texture_slot'], material_struct[i]['textures'][j]['unk_01'], material_struct[i]['textures'][j]['unk_02'])
+                + struct.pack("<3i", material_struct[i]['textures'][j]['texture_slot'],\
+                material_struct[i]['textures'][j]['unk_01'], material_struct[i]['textures'][j]['unk_02'])
+            if kuro_ver > 1:
+                texture_blocks += struct.pack("<2i", material_struct[i]['textures'][j]['unk_03'],\
+                    material_struct[i]['textures'][j]['unk_04'])
             texture_block_count += 1
         material_block += struct.pack("<I", texture_block_count) + texture_blocks
         shader_elements = bytes()
@@ -77,7 +84,7 @@ def build_material_section (mdl_filename):
         material_switch_count = 0
         for j in range(len(material_struct[i]['material_switches'])):
             material_switches += make_pascal_string(material_struct[i]['material_switches'][j]['material_switch_name']) \
-                + struct.pack("<I", material_struct[i]['material_switches'][j]['int2'])
+                + struct.pack("<i", material_struct[i]['material_switches'][j]['int2'])
             material_switch_count += 1
         material_block += struct.pack("<I", material_switch_count) + material_switches
         material_block += struct.pack("<I{0}B".format(len(material_struct[i]['uv_map_indices'])), len(material_struct[i]['uv_map_indices']), *material_struct[i]['uv_map_indices'])
@@ -86,16 +93,19 @@ def build_material_section (mdl_filename):
         output_buffer += material_block
     return(struct.pack("<2I", 0, len(output_buffer)) + output_buffer)
 
-def build_mesh_section (mdl_filename):
+def build_mesh_section (mdl_filename, kuro_ver = 1):
     try:
         mesh_struct = read_struct_from_json(mdl_filename + "/mesh_info.json")
     except:
         with open(mdl_filename + '.mdl', "rb") as f:
             mdl_data = f.read()
         mdl_data = decryptCLE(mdl_data)
-        mesh_data = isolate_mesh_data(mdl_data)
-        mesh_struct = obtain_mesh_data(mesh_data)["mesh_blocks"]
+        mesh_struct = obtain_mesh_data(mdl_data)["mesh_blocks"]
     output_buffer = struct.pack("<I", len(mesh_struct))
+    if kuro_ver > 1:
+        prim_output_header = bytes()
+        prim_output_data = bytes()
+        prim_buffer_count = 0
     for i in range(len(mesh_struct)):
         mesh_block = bytes()
         meshes = 0 # Keep count of actual meshes imported, in case some have been deleted
@@ -105,7 +115,11 @@ def build_mesh_section (mdl_filename):
                 fmt = read_fmt(mesh_filename + '.fmt')
                 ib = list(chain.from_iterable(read_ib(mesh_filename + '.ib', fmt)))
                 vb = read_vb(mesh_filename + '.vb', fmt)
-                primitive_buffer = struct.pack("<2I", mesh_struct[i]["primitives"][j]["material_offset"], len(vb)+1)
+                primitive_buffer = struct.pack("<I", mesh_struct[i]["primitives"][j]["material_offset"])
+                if kuro_ver == 1:
+                    primitive_buffer += struct.pack("<I", len(vb)+1)
+                elif kuro_ver > 1:
+                    primitive_buffer += struct.pack("<2I", len(ib), mesh_struct[i]["primitives"][j]["unk"])
                 for k in range(len(vb)):
                     match vb[k]["SemanticName"]:
                         case "POSITION":
@@ -133,12 +147,22 @@ def build_mesh_section (mdl_filename):
                         case "UINT":
                             element_type = 'I' # Assuming 32-bit since Kuro models all use 32-bit
                     raw_buffer = struct.pack("<{0}{1}".format(vec_elements*len(vb[k]["Buffer"]), element_type), *list(chain.from_iterable(vb[k]["Buffer"])))
-                    primitive_buffer += struct.pack("<3I", type_int, len(raw_buffer), vec_stride) + raw_buffer
+                    if kuro_ver == 1:
+                        primitive_buffer += struct.pack("<3I", type_int, len(raw_buffer), vec_stride) + raw_buffer
+                    elif kuro_ver > 1:
+                        prim_output_header += struct.pack("<5I", type_int, len(raw_buffer), vec_stride, i, j)
+                        prim_output_data += raw_buffer
+                        prim_buffer_count += 1
                 # After VB, need to add IB
                 # Making assumptions here that it will always be in Rxx_UINT format, saves a bunch of code
                 vec_stride = int(int(re.findall("[0-9]+",fmt["format"].split('DXGI_FORMAT_')[-1].split('_')[0])[0]) / 8)
                 raw_ibuffer = struct.pack("<{0}I".format(len(ib), element_type), *ib)
-                primitive_buffer += struct.pack("<3I", 7, len(raw_ibuffer), vec_stride) + raw_ibuffer
+                if kuro_ver == 1:
+                    primitive_buffer += struct.pack("<3I", 7, len(raw_ibuffer), vec_stride) + raw_ibuffer
+                elif kuro_ver > 1:
+                    prim_output_header += struct.pack("<5I", 7, len(raw_ibuffer), vec_stride, i, j)
+                    prim_output_data += raw_ibuffer
+                    prim_buffer_count += 1
                 mesh_block += primitive_buffer
                 meshes += 1
             except:
@@ -158,16 +182,22 @@ def build_mesh_section (mdl_filename):
         section2_block = struct.pack("<I", len(raw_section2)) + raw_section2
         mesh_block = make_pascal_string(mesh_struct[i]["name"]) + struct.pack("<I", len(mesh_block)) + mesh_block + section2_block
         output_buffer += mesh_block
-    return(struct.pack("<2I", 1, len(output_buffer)) + output_buffer)
+        mesh_section_buffer = struct.pack("<2I", 1, len(output_buffer)) + output_buffer
+        primitive_section_buffer = bytes()
+        if kuro_ver > 1: # Primitives in a separate section #4
+            primitive_output_buffer = struct.pack("<I", prim_buffer_count) + prim_output_header + prim_output_data
+            primitive_section_buffer += struct.pack("<2I", 4, len(primitive_output_buffer)) + primitive_output_buffer
+    return(mesh_section_buffer, primitive_section_buffer)
 
 def process_mdl (mdl_file):
     with open(mdl_file, "rb") as f:
         mdl_data = f.read()
     print("Processing {0}...".format(mdl_file))
     mdl_data = decryptCLE(mdl_data)
-    material_data = build_material_section(mdl_file[:-4])
-    mesh_data = build_mesh_section(mdl_file[:-4])
-    new_mdl_data = insert_model_data(mdl_data, material_data, mesh_data)
+    kuro_ver = get_kuro_ver(mdl_data)
+    material_data = build_material_section(mdl_file[:-4], kuro_ver = kuro_ver)
+    mesh_data, primitive_data = build_mesh_section(mdl_file[:-4], kuro_ver = kuro_ver)
+    new_mdl_data = insert_model_data(mdl_data, material_data, mesh_data, primitive_data)
     # Instead of overwriting backups, it will just tag a number onto the end
     backup_suffix = ''
     if os.path.exists(mdl_file + '.bak' + backup_suffix):
