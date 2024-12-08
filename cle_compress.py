c2e8d2cc11fe8eeec46f0433f6aef1f8ba8ab469
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2024-12-08 08:55:34 -0800

    Add encryption
    Thank you to the authors of [KuroTools](https://github.com/nnguyen259/KuroTools)!

--- cle_compress.py
+++ cle_compress.py
@@ -1,4 +1,4 @@
-# Tool to compress ED9_@ / Kuro no Kiseki 2 assets for Kuro 2 CLE.
+# Tool to compress (and encrypt) ED9_2 / Kuro no Kiseki 2 assets for Kuro 2 CLE.
 # Usage:  Run by itself without commandline arguments and it will compress all files in the folder.
 # For command line options (including option to dump vertices), run:
 # /path/to/python3 kuro2_compress.py --help
@@ -9,34 +9,48 @@
 #
 # GitHub eArmada8/misc_kiseki
 
-# Thank you to authors of Kuro Tools for this decrypt function
+# Thank you to authors of Kuro Tools for the encryption and compression functions
 # https://github.com/nnguyen259/KuroTools
 
 try:
-    import zstandard, struct, shutil, os, sys, glob
+    import blowfish, zstandard, struct, operator, shutil, os, sys, glob
 except ModuleNotFoundError as e:
     print("Python module missing! {}".format(e.msg))
     input("Press Enter to abort.")
     raise   
 
+key = b"\x16\x4B\x7D\x0F\x4F\xA7\x4C\xAC\xD3\x7A\x06\xD9\xF8\x6D\x20\x94"
+IV = b"\x9D\x8F\x9D\xA1\x49\x60\xCC\x4C"
+iv = struct.unpack(">Q", IV)
+
 def compressCLE(file_content):
     magic = file_content[0:4]
-    compressed_magic = b"D9BA"
     result = file_content
-    if not magic == compressed_magic: # Don't compress files that are already compressed:
-        compressor = zstandard.ZstdCompressor(level = 12, write_checksum = True)
-        result = compressor.compress(file_content)
-        while (len(result) % 8) > 0:
-            result += b'\x00'
-        result = compressed_magic + struct.pack("<I", len(result)) + result
+    compressor = zstandard.ZstdCompressor(level = 9, write_checksum = True)
+    result = compressor.compress(file_content)
+    while (len(result) % 8) > 0:
+        result += b'\x00'
+    result = b"D9BA" + struct.pack("<I", len(result)) + result
+    return result
+
+def encryptCLE(file_content):
+    cipher = blowfish.Cipher(key, byte_order = "big")
+    dec_counter = blowfish.ctr_counter(iv[0], f = operator.add)
+    result = b"".join(cipher.encrypt_ctr(file_content, dec_counter))
+    while (len(result) % 8) > 0:
+        result += b'\x00'
+    result = b"F9BA" + struct.pack("<I", len(result)) + result
     return result
 
 def processfile(filename):
     with open(filename, "rb") as f:
         file_content = f.read()
     print("Processing {0}...".format(filename))
-    new_file_content = compressCLE(file_content)
-    if not new_file_content == file_content: # Do not process if file is unchanged
+    skip_magic = [b"D9BA", b"F9BA", b"C9BA"]
+    if not file_content[0:4] in skip_magic: # Don't process files that are already CLE format:
+        new_file_content = compressCLE(file_content)
+        if not filename.split('.')[-1] in ['mdl','dds']:
+            new_file_content = encryptCLE(new_file_content)
         # Instead of overwriting backups, it will just tag a number onto the end
         backup_suffix = ''
         if os.path.exists(filename + '.bak' + backup_suffix):
